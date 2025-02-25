24243ec225f5afbc98b0ed7a4ac56eee9202b8c0
Wendy Hemlock <wendyhemlock@gmail.com>
2025-02-25 01:01:53 -0400

    fix: Home now redirects to home

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,20 +1,39 @@
 <template>
   <v-app-bar>
     <div class="header">
-      <v-toolbar-title>
-        <v-icon left>mdi-briefcase-variant</v-icon>
-        Jobzen
+      <v-toolbar-title @click="goHome">
+        <div class="title">
+          <v-icon left>mdi-briefcase-variant</v-icon>
+          <span>JobZen</span>
+        </div>
       </v-toolbar-title>
 
       <div class="button-group">
-        <v-btn text="true" variant="flat" color="primary"> Log In </v-btn>
-        <v-btn text="true">Sign Up</v-btn>
+        <v-btn variant="flat" color="primary" to="/login"> Log In </v-btn>
+        <v-btn text="true" to="/signup">Sign Up</v-btn>
       </div>
     </div>
   </v-app-bar>
 </template>
 
+<script setup>
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const goHome = () => {
+  router.push("/");
+};
+</script>
+
 <style>
+.title {
+  cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 5px;
+}
+
 .header {
   width: 100%;
   display: flex;
